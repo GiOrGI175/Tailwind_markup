7647b88bd249b90d2c53062e38d28a5aa81169cf
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-09-29 13:31:49 +0400

    feature/main
    adding main section without differnt movies img

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,14 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.mask-gradient': {
+          '-webkit-mask-image': 'linear-gradient(#000, rgba(0, 0, 0, 0))',
+          'mask-image': 'linear-gradient(#000, rgba(0, 0, 0, 0))',
+        },
+      });
+    },
+  ],
 };
