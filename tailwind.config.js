6d741605c29e4796034956cd2e3d0de1c5e43a73
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-10-01 21:27:13 +0400

    feature/section3

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       backgroundImage: {
         'custom-gradient':
           'linear-gradient(to right top, #0f0f0f, #0f0f0f, #0f0f0f, #371414);',
+        'border-gradiant':
+          'linear-gradient(to right, #fea7a7, #ff8f8d, #fd7771, #f95e54, #f24236, #f24236, #f24236, #f24236, #f95e54, #fd7771, #ff8f8d, #fea7a7);',
+      },
+
+      borderImage: {
+        gradient:
+          'linear-gradient(90deg, rgba(229, 0, 0, 0) 0%, #E50000 16.67%, rgba(229, 0, 0, 0) 100%)',
       },
     },
   },
@@ -15,6 +22,14 @@ module.exports = {
         '.mask-gradient': {
           '-webkit-mask-image': 'linear-gradient(#000, rgba(0, 0, 0, 0))',
           'mask-image': 'linear-gradient(#000, rgba(0, 0, 0, 0))',
+        },
+
+        '.border-gradient': {
+          border: '1px solid transparent',
+          'border-image-source':
+            'linear-gradient(90deg, rgba(229, 0, 0, 0) 0%, #E50000 16.67%, rgba(229, 0, 0, 0) 100%)',
+          'border-image-slice': '1',
+          'border-image-width': '1',
         },
       });
     },
