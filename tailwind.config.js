c1da669360f911ec4bf6efdf5ec1277dc42ccf76
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-10-04 11:15:31 +0400

    feature/hader-responsiv

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,15 @@ module.exports = {
           'linear-gradient(to right top, #0f0f0f, #0f0f0f, #0f0f0f, #371414);',
         'border-gradiant':
           'linear-gradient(to right, #fea7a7, #ff8f8d, #fd7771, #f95e54, #f24236, #f24236, #f24236, #f24236, #f95e54, #fd7771, #ff8f8d, #fea7a7);',
+        'header-gradient':
+          'background-image: linear-gradient(to right bottom, #141414, #3d3d3d, #6b6a6a, #9d9c9c, #d1d1d0);',
       },
       screens: {
         'max-xs': { max: '480px' },
         'max-ss': { max: '620px' },
         'max-sm': { max: '768px' },
         'max-md': { max: '1060px' },
-        'max-lg': { max: '1200px' },
+        'max-lg': { max: '1440px' },
         'max-xl': { max: '1700px' },
       },
     },
@@ -25,6 +27,13 @@ module.exports = {
         '.mask-gradient': {
           '-webkit-mask-image': 'linear-gradient(#000, rgba(0, 0, 0, 0))',
           'mask-image': 'linear-gradient(#000, rgba(0, 0, 0, 0))',
+        },
+
+        '.mask-gradient-2': {
+          '-webkit-mask-image':
+            'linear-gradient(to bottom, rgba(255, 0, 0, 0), #ff0000, rgba(0, 0, 0, 0))',
+          'mask-image':
+            'linear-gradient(to bottom, rgba(255, 0, 0, 0), #ff0000, rgba(0, 0, 0, 0))',
         },
 
         '.border-gradient': {
